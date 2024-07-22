d31571efab3612e06ce8349d7797f801b3e4f8fe
CodewithSiddd <siddheshvarework@gmail.com>
2024-07-22 22:21:37 +0530

    Create TwoSum.java

---
+++ arrays/TwoSum.java
@@ -0,0 +1,15 @@
+//Leetcode question - Two sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    // int[] num = num.add(i)
+                    return new int[]{i,j};
+                }
+            }
+        }
+    return new int[]{};
+    }
+}
