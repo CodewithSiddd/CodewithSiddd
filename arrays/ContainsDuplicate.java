0b3e6f66c45007c3cef23707d7d8729e504f1496
CodewithSiddd <siddheshvarework@gmail.com>
2024-07-23 22:27:54 +0530

    Contains Duplicate

---
+++ arrays/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+//Leetcode question - Contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> set = new HashSet<>();
+       for(int i : nums) {
+           if(!set.contains(i)){
+               set.add(i);
+           }
+           else {
+               return true;
+           }
+       }
+       return false;
+    }
+}
