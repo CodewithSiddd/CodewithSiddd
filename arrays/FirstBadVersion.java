4792d4fdcfb70d7dbd51f61387c0f0091d77f4f5
CodewithSiddd <siddheshvarework@gmail.com>
2024-07-24 14:30:30 +0530

    First Bad Version

---
+++ arrays/FirstBadVersion.java
@@ -0,0 +1,21 @@
+//Leetcode question - First Bad Version
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 0, end = n;
+        int badVersion = 0;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid)){
+                badVersion = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return badVersion;
+    }
+}
