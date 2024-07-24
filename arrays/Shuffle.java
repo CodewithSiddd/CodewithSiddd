7ddcfeff4a70cd8ea1c7be4c154b441c5f91c45e
CodewithSiddd <siddheshvarework@gmail.com>
2024-07-24 14:15:50 +0530

    Shuffle the Array

---
+++ arrays/Shuffle.java
@@ -0,0 +1,14 @@
+//Leetcode question - Shuffle the Array
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            ans[j] = nums[i];
+            ans[j + 1] = nums[i + n];
+            j = j + 2;
+        }
+        return ans;
+    }
+}
