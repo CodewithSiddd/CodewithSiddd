9d79b173a09a173148837425dc8b66918fe6f131
CodewithSiddd <siddheshvarework@gmail.com>
2024-07-24 18:02:48 +0530

    Move Zeroes

---
+++ arrays/MoveZeroes.java
@@ -0,0 +1,16 @@
+//Leetcode question - Move Zeroes
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j : nums) {
+            if(j != 0) {
+                nums[i++] = j;
+            }
+        }
+
+        while(i < nums.length) {
+            nums[i++] = 0;
+        }
+    }
+}
